ebb6e43718bd8b533772cb5f5e1ea8fd25f10c07
Norman Hooper <nhooper@dimagi.com>
2022-04-13 10:57:46 +0100

    Pull out HQ_BASE_URL const

--- superset_config.py
+++ superset_config.py
@@ -1,11 +1,13 @@
+from urllib.parse import urljoin
+
 import hq_superset
 from flask_appbuilder.security.manager import AUTH_OAUTH
 from superset import config as superset_config
 
-
 hq_superset.patch_superset_config(superset_config)
 
 
+HQ_BASE_URL = 'http://127.0.0.1:8000/'
 AUTH_TYPE = AUTH_OAUTH
 OAUTH_PROVIDERS = [
     {
@@ -14,9 +16,9 @@ OAUTH_PROVIDERS = [
         'remote_app': {
             'client_id': '',
             'client_secret': '',
-            'api_base_url': 'http://127.0.0.1:8000/',
-            'access_token_url': 'http://127.0.0.1:8000/oauth/token/',
-            'authorize_url': 'http://127.0.0.1:8000/oauth/authorize/',
+            'api_base_url': HQ_BASE_URL,
+            'access_token_url': urljoin(HQ_BASE_URL, '/oauth/token/'),
+            'authorize_url': urljoin(HQ_BASE_URL, '/oauth/authorize/'),
             'client_kwargs':{
                 'scope': 'reports:view access_apis'
             },
@@ -25,8 +27,8 @@ OAUTH_PROVIDERS = [
 ]
 
 
-
-# Will allow user self registration, allowing to create Flask users from Authorized User
+# Will allow user self registration, allowing to create Flask users from
+# Authorized User
 AUTH_USER_REGISTRATION = True
 
 # The default user self registration role
