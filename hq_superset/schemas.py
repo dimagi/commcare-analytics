8cf06e1d6df34299c1451cbee2ca50eb333db823
Norman Hooper <nhooper@dimagi.com>
2022-04-13 10:59:42 +0100

    Define schema for DataSourceConfig

---
+++ hq_superset/schemas.py
@@ -0,0 +1,14 @@
+from datetime import timezone
+
+from marshmallow import Schema, fields
+
+
+class HQDataSourceConfiguration(Schema):
+    domain = fields.Str(required=True)
+    id = fields.Str(required=True)
+    display_name = fields.Str(required=True, allow_none=True)
+    retrieved_at = fields.NaiveDateTime(
+        format='iso',
+        timezone=timezone.utc,
+        required=True,
+    )
