bc257b4ae61e3c02d46a69909513bbdf1e1c8bc8
Norman Hooper <nhooper@dimagi.com>
2022-04-14 00:49:53 +0100

    Dummy view

--- hq_superset/views.py
+++ hq_superset/views.py
@@ -1,8 +1,9 @@
-from flask import url_for, render_template, redirect, request
-from flask_appbuilder import expose, BaseView
+from flask import redirect, request
+from flask_appbuilder import BaseView, expose
 from flask_appbuilder.security.decorators import has_access, permission_name
 from flask_login import current_user
-import superset
+from superset.typing import FlaskResponse
+
 from . import hq_domain
 
 
@@ -37,3 +38,27 @@ class SelectDomainView(BaseView):
         # Todo validate domain permission
         response.set_cookie('hq_domain', hq_domain)
         return response
+
+
+class HQDataSourceConfigView(BaseView):
+    # TODO: Extend SupersetModelView instead
+    # datamodel = SQLAInterface(HQDataSourceConfigModel)
+    # TODO: On login, pull data sources and cache them in a Model.
+
+    route_base = '/hq_data_source'
+    class_permission_name = 'Superset'
+
+    @has_access
+    @expose('/list/', methods=['GET'])
+    @permission_name("profile")  # TODO: What is this for?
+    def list(self) -> FlaskResponse:
+        # return super().render_app_template()
+
+        # domain = 'demo'
+        # data_source_configs = fetch_data_source_configs(domain)
+
+        data_source_configs = ['foo', 'bar', 'baz']
+        return self.render_template(
+            'hq_data_source_config_list.html',
+            data_source_configs=data_source_configs,
+        )
