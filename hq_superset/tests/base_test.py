83c6fb725c1282c9c6c5b40b676c4d09d1606bc7
Sravan Reddy <sravfeyn@gmail.com>
2023-01-04 12:51:31 +0530

    Test datasource CRUD

--- hq_superset/tests/base_test.py
+++ hq_superset/tests/base_test.py
@@ -11,36 +11,34 @@ from flask_appbuilder import SQLA
 from flask_testing import TestCase
 from superset.app import create_app
 from hq_superset.utils import get_hq_database, DOMAIN_PREFIX
+from .utils import setup_hq_db
 
 superset_test_home = os.path.join(os.path.dirname(__file__), ".test_superset")
 shutil.rmtree(superset_test_home, ignore_errors=True)
 os.environ["SUPERSET_HOME"] = superset_test_home
 os.environ["SUPERSET_CONFIG"] = "hq_superset.tests.config_for_tests.superset_config"
-app = create_app()
+test_app = create_app()
 
 
 class SupersetTestCase(TestCase):
     """The base TestCase class for all tests relying on Flask app context"""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.maxDiff = None
-        self.db = SQLA()
-
     def create_app(self):
-        return app
+        return test_app
 
     def setUp(self):
-        self.db.create_all()
+        import superset
+        superset.db.create_all()
 
 
 class HQDBTestCase(SupersetTestCase):
 
     def setUp(self):
         super(HQDBTestCase, self).setUp()
+        setup_hq_db()
         self.hq_db = get_hq_database()
 
     def tearDown(self):
+        # Drop HQ DB Schemas
         engine = self.hq_db.get_sqla_engine()
         with engine.connect() as connection:
             results = connection.execute(text("SELECT schema_name FROM information_schema.schemata"))
--- hq_superset/tests/utils.py
+++ hq_superset/tests/utils.py
@@ -34,3 +34,121 @@ def setup_hq_db():
                 'database_name': HQ_DB_CONNECTION_NAME
             }
         ).run()
+
+
+TEST_DATASOURCE = {
+  "configured_filter": {
+    "filters": [
+      {
+        "comment": None,
+        "expression": {
+          "datatype": None,
+          "property_name": "xmlns",
+          "type": "property_name"
+        },
+        "operator": "eq",
+        "property_value": "http://openrosa.org/formdesigner/B7F9A7EA-E310-4673-B7DA-423BE63A34AA",
+        "type": "boolean_expression"
+      },
+      {
+        "comment": None,
+        "expression": {
+          "datatype": None,
+          "property_name": "app_id",
+          "type": "property_name"
+        },
+        "operator": "eq",
+        "property_value": "2cb1a465c85644b8a21756c450c3e886",
+        "type": "boolean_expression"
+      }
+    ],
+    "type": "and"
+  },
+  "configured_indicators": [
+    {
+      "column_id": "data_visit_date_eaece89e",
+      "comment": None,
+      "create_index": False,
+      "datatype": "date",
+      "display_name": "visit_date",
+      "expression": {
+        "datatype": None,
+        "property_path": [
+          "form",
+          "visit_date"
+        ],
+        "type": "property_path"
+      },
+      "is_Noneable": True,
+      "is_primary_key": False,
+      "transform": {},
+      "type": "expression"
+    },
+    {
+      "column_id": "data_visit_number_33d63739",
+      "comment": None,
+      "create_index": False,
+      "datatype": "integer",
+      "display_name": "visit_number",
+      "expression": {
+        "datatype": None,
+        "property_path": [
+          "form",
+          "visit_number"
+        ],
+        "type": "property_path"
+      },
+      "is_Noneable": True,
+      "is_primary_key": False,
+      "transform": {},
+      "type": "expression"
+    },
+    {
+      "column_id": "data_lmp_date_5e24b993",
+      "comment": None,
+      "create_index": False,
+      "datatype": "date",
+      "display_name": "lmp_date",
+      "expression": {
+        "datatype": None,
+        "property_path": [
+          "form",
+          "lmp_date"
+        ],
+        "type": "property_path"
+      },
+      "is_Noneable": True,
+      "is_primary_key": False,
+      "transform": {},
+      "type": "expression"
+    },
+    {
+      "column_id": "data_visit_comment_fb984fda",
+      "comment": None,
+      "create_index": False,
+      "datatype": "string",
+      "display_name": "visit_comment",
+      "expression": {
+        "datatype": None,
+        "property_path": [
+          "form",
+          "visit_comment"
+        ],
+        "type": "property_path"
+      },
+      "is_Noneable": True,
+      "is_primary_key": False,
+      "transform": {},
+      "type": "expression"
+    }
+  ],
+  "display_name": "ANC visit (v3) 2020-12-16 04:49:22",
+  "id": "test1_ucr1",
+  "resource_uri": "/a/demo/api/v0.5/ucr_data_source/52a134da12c9b801bd85d2122901b30c/"
+}
+
+TEST_UCR_CSV = """\
+doc_id,inserted_at,data_visit_date_eaece89e,data_visit_number_33d63739,data_lmp_date_5e24b993,data_visit_comment_fb984fda
+a1, 2021-12-20, 2022-01-19, 100, 2022-02-20, some_text
+a2, 2021-12-22, 2022-02-19, 10, 2022-03-20, some_other_text
+"""
