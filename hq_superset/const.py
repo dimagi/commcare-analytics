db99e5cc5eb408153392c7f913ff7a143fe11258
Charl Smit <csmit@dimagi.com>
2024-08-20 12:31:44 +0200

    Add HQ role name mapper

--- hq_superset/const.py
+++ hq_superset/const.py
@@ -2,3 +2,9 @@
 HQ_DATABASE_NAME = "HQ Data"
 
 OAUTH2_DATABASE_NAME = "oauth2-server-data"
+
+HQ_ROLE_NAME_MAPPING = {
+    "gamma": "Gamma",
+    "dataset_editor": "dataset_editor",
+    "sql_lab": "sql_lab",
+}
