df84a610a11dc08ff371192fd7c7d714389dc350
Charl Smit <csmit@dimagi.com>
2024-12-12 10:57:52 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='hq_superset',
-    version='1.0.0',
+    version='1.1.0',
     description='CommCare HQ Superset Integration',
     license='Apache2',
     author='Dimagi Inc.',
